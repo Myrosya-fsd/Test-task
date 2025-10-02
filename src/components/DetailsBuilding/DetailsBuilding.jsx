14b521b6bbe6980d29e84622d096f85327ed4462
myroslava <murosja80@gmail.com>
2025-10-02 19:01:55 +0300

    update_name_img

--- src/components/DetailsBuilding/DetailsBuilding.jsx
+++ src/components/DetailsBuilding/DetailsBuilding.jsx
@@ -4,11 +4,7 @@ const DetailsBuilding = ({ year, type, img, name, adress, tags }) => {
   return (
     <div style={{ width: "440px" }}>
       <div className={styles.card}>
-        <img
-          className={styles.imgTop}
-          src="/public/img/Pin.png"
-          alt="torn paper"
-        />
+        <img className={styles.imgTop} src="/public/img/Pin.png" alt="pin" />
 
         <div className={styles.cardTop}>
           <p className={styles.year}>{year}</p>
