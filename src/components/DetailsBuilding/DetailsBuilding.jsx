5326e2ae049eca1a234cb550011df92cd3e88d26
myroslava <murosja80@gmail.com>
2025-10-02 19:12:15 +0300

    Normalize line endings

--- src/components/DetailsBuilding/DetailsBuilding.jsx
+++ src/components/DetailsBuilding/DetailsBuilding.jsx
@@ -4,7 +4,7 @@ const DetailsBuilding = ({ year, type, img, name, adress, tags }) => {
   return (
     <div style={{ width: "440px" }}>
       <div className={styles.card}>
-        <img className={styles.imgTop} src="/public/img/Pin.png" alt="pin" />
+        <img className={styles.imgTop} src="/img/Pin.png" alt="pin" />
 
         <div className={styles.cardTop}>
           <p className={styles.year}>{year}</p>
@@ -27,11 +27,7 @@ const DetailsBuilding = ({ year, type, img, name, adress, tags }) => {
           ))}
         </div>
       </div>
-      <img
-        className={styles.imgBottom}
-        src="/public/img/Down.png"
-        alt="torn paper"
-      />
+      <img className={styles.imgBottom} src="/img/Down.png" alt="torn paper" />
     </div>
   );
 };
